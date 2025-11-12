e63dd0ec706f015bd191c1e6248879aa6cc4bac1
Nicolas Acuña <niconiampira@hotmail.com>
2025-11-11 23:21:34 -0500

    Refactor MainLayout: Remove container constraints for full-width layouts

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -13,7 +13,8 @@ export function MainLayout() {
       <Header />
 
       <main id="main-content" className="flex-1" tabIndex={-1}>
-        <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+        {/* <div className="px-4 py-8 sm:px-6 lg:px-8"> */}
+          <div>
           <Breadcrumbs />
           <Outlet />
         </div>
